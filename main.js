ff2f366a7334ffb4654cd66939ea2c95f026c39b
olivier-jnk <oliviertrouvain01@gmail.com>
2024-01-22 11:00:45 +0100

    survol

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ const c = document.getElementById("celian")
 c.addEventListener("mouseover", (event) => {
     console.log(c.textContent)
 });
+
+const f = document.getElementById("flavie")
+f.addEventListener("mouseover", (event) => {
+    console.log(f.textContent)
+});
+
+const o = document.getElementById("olivier")
+o.addEventListener("mouseover", (event) => {
+    console.log(o.textContent)
+});
+
+//trouver moyen de condenser tout ca.
+
+ul = document.getElementById("lul")
+console.log(ul.length)
